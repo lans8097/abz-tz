a8940a501bf0c56535fae926becc579a5ca4d20a
= <=>
2018-01-13 01:36:47 +0200

    add package IdeHelper, Debugbar and basic auth

--- config/app.php
+++ config/app.php
@@ -163,6 +163,10 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+		
+		//dev
+		Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+		Barryvdh\Debugbar\ServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -225,6 +229,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+		//dev
+		'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
